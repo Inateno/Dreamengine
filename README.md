3b9e3236e2d98516c56d505b5ac01517d2fe8c73
inateno <inateno@gmail.com>
2015-09-02 19:45:55 +0200

    website first commit

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
-Dreamengine
+![DreamEngine Logo](http://dreamengine.dreamirl.com/assets/imgs/logo.png)
+
+Dream-Engine - Website
+===========
+Here you can find sources used for the website, I'm not a big expert in "cool" UI design, but I wanted to keep something simple, only HTML is good for me.
+Don't hesitate to improve it and doing PR ;)
+
+Contributing
+-------
+
+[The "big" how to here](http://dreamengine.dreamirl.com/#howto).
+If you want write more tutorials you are welcome.
+
+You want create a translation for the doc or the website, have a better UI design suggestion, fix mistakes ?
+Cool ! Do a PR :)
+
+Credits
 ===========
 
-A powerfull JS 2D Game Engine cross-compatible and easily extendable
+The engine is made by Rogliano Antoine aka [Inateno](http://inateno.com)
+The official website [HTML5-Dreamengine](http://dreamengine.dreamirl.com) if you wanna help me on this I say hell yes.
+You can also follow me on [Twitter](http://twitter.com/inateno)
+
+Also I'm streaming on Twitch sometimes about "making games with Dreamengine", search Inateno.
+
+License
+-------
+
+The Dreamengine is released under the [MIT License](http://opensource.org/licenses/MIT).
+
+Finally it's here for everyone, feel free to use it, share it and contribute.
+
+Feel free to contact me, I'm on Twitter @Inateno or the team @DreamirlGames (Dreamirl on Facebook)
---
+++ site/js/source.js
@@ -0,0 +1,59 @@
+﻿$( window ).load( init );
+
+function init()
+{
+  $( ".btn-default.about" ).click( function( e )
+  {
+    var parent = $( e.currentTarget ).parent();
+    while ( !parent.hasClass( "card" ) )
+      parent = parent.parent();
+    
+    parent.toggleClass( 'reverse' );
+  } );
+  
+  var $pages = $( '.page' );
+  $pages.hide( 0 );
+  
+  var $links = $( 'a[data-target]' );
+  var currentPage = "home";
+  $links.click( function( e )
+  {
+    var $el = $( "a[data-target=" + $( e.currentTarget ).attr( "data-target" ) + "]" );
+    $links.parent().removeClass( "active" );
+    $el.parent().addClass( "active" );
+    
+    if ( currentPage == $el.attr( "data-target" ) )
+      return;
+    currentPage = $el.attr( "data-target" );
+    $pages.hide( 200 );
+    $( '.page.' + currentPage ).show( 200 );
+  } );
+  
+  $( $links[ 0 ] ).click();
+  
+  $( '.learnMore' ).click( function( e )
+  {
+    $( 'li>a.more' ).click();
+  } );
+  
+  $( window ).on( 'scroll', function()
+  {
+    if ( $( 'body' ).scrollTop() > 130 )
+    {
+      $( '.navbar-fixed-top' ).addClass( "onscreen" );
+    }
+    else
+    {
+      $( '.navbar-fixed-top' ).removeClass( "onscreen" );
+    }
+  } );
+  
+  var hash = window.location.hash.replace('#','').toLowerCase();
+  var routes = [ "engine", "games", "howto", "doc" ];
+  if ( routes.indexOf( hash ) == -1 )
+    hash = routes[ 0 ];
+  $pages.hide( true );
+  $( '.page.' + hash ).show( true );
+  currentPage = hash;
+  $( "a[data-target=" + hash + "]" ).click();
+}
